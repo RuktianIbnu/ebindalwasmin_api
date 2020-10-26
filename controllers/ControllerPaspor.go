13e672841f146edcb655f8e825e26dfd44c0c982
Fajar Lubis <fajarsidiqlubis13@gmail.com>
2020-10-26 11:55:55 +0700

    fajar-dev

--- controllers/ControllerPaspor.go
+++ controllers/ControllerPaspor.go
@@ -1,13 +1,17 @@
 package controllers
 
 import (
+	"ebindalwasmin_api/models"
 	"fmt"
 	"net/http"
-	"../models"
-	//"../config"
+
 	"encoding/json"
+
 	jwt "github.com/dgrijalva/jwt-go"
+
+	// _ "github.com/go-sql-driver/mysql"
 	_ "github.com/go-sql-driver/mysql"
+
 	//"io/ioutil"
 	"log"
 )
@@ -18,11 +22,12 @@ var pasporDataDetail models.PasporDataDetail
 var statusResPaspor models.StatusResPaspor
 var result = statusResPaspor
 
+// CreateDataPaspor ...
 func CreateDataPaspor(w http.ResponseWriter, r *http.Request) {
 
 	//db := config.Connect()
-	
-    if r.Method == "POST" {
+
+	if r.Method == "POST" {
 
 		tokenString := r.Header.Get("Authorization")
 		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
@@ -37,34 +42,34 @@ func CreateDataPaspor(w http.ResponseWriter, r *http.Request) {
 			err := json.NewDecoder(r.Body).Decode(&pasporDatas)
 			log.Println(pasporDatas)
 			if err != nil {
-				statusResPaspor.Status	= 500
-				statusResPaspor.Msg		= "data kosong"
+				statusResPaspor.Status = 500
+				statusResPaspor.Msg = "data kosong"
 
 				result = statusResPaspor
-			}else{
+			} else {
 				//createData, err := db.Prepare("INSERT INTO data_paspor(id_jenis, id_user, id_kantor, tanggal, laki, perempuan, total, id_wilayah_kerja) VALUES(?,?,?,?,?,?,?,?)")
 				if err != nil {
-					statusResPaspor.Status	= 400
-					statusResPaspor.Msg		= "gagal tambah data"
+					statusResPaspor.Status = 400
+					statusResPaspor.Msg = "gagal tambah data"
 
 					result = statusResPaspor
 					panic(err.Error())
-					
+
 				} else {
-					statusRes.Status	= 200
-					statusRes.Msg		= "berhasil tambah data"
+					statusRes.Status = 200
+					statusRes.Msg = "berhasil tambah data"
 					result = statusResPaspor
 				}
 				w.Header().Set("Content-Type", "application/json")
 				//w.Write(b)
 			}
 		} else {
-			statusRes.Status	= 400
-			statusRes.Msg		= "not authorized"
-			statusRes.Token		= ""
+			statusRes.Status = 400
+			statusRes.Msg = "not authorized"
+			statusRes.Token = ""
 
 			result = statusResPaspor
 		}
-		
+
 	}
 }
