5b4e142e4212cacb8d3cd9b3aedfbcaa600db710
Fajar Lubis <fajarsidiqlubis13@gmail.com>
2020-11-08 20:42:52 +0700

    hapus route ping di router

--- router/router.go
+++ router/router.go
@@ -19,12 +19,12 @@ func Routes() *gin.Engine {
 	r := gin.Default()
 	r.Use(cors.Default())
 
-	// for health check purpose only
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	// // for health check purpose only
+	// r.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(200, gin.H{
+	// 		"message": "pong",
+	// 	})
+	// })
 
 	userHandler := uh.NewHandler()
 	generalHandler := gh.NewHandler()
