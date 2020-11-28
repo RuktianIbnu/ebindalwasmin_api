24961e69210f11a1326e66200aaf0a10500735b5
RuktianIbnu <ruktianibnu95@gmail.com>
2020-11-29 00:27:00 +0700

    comment router

--- router/router.go
+++ router/router.go
@@ -1,15 +1,6 @@
 package router
 
 import (
-	gh "ebindalwasmin_api/handler/general"
-	ih "ebindalwasmin_api/handler/intal"
-	kh "ebindalwasmin_api/handler/kategoripnbp"
-	sh "ebindalwasmin_api/handler/master"
-	ph "ebindalwasmin_api/handler/paspor"
-	pnbph "ebindalwasmin_api/handler/pnbp"
-	uh "ebindalwasmin_api/handler/user"
-	vh "ebindalwasmin_api/handler/visa"
-
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -26,45 +17,45 @@ func Routes() *gin.Engine {
 		})
 	})
 
-	userHandler := uh.NewHandler()
-	generalHandler := gh.NewHandler()
-	kategoriPNBPHandler := kh.NewHandler()
-	pasporHandler := ph.NewHandler()
-	visaHandler := vh.NewHandler()
-	intalHandler := ih.NewHandler()
-	pnbpHandler := pnbph.NewHandler()
-	satkerHandler := sh.NewHandler()
+	// userHandler := uh.NewHandler()
+	// generalHandler := gh.NewHandler()
+	// kategoriPNBPHandler := kh.NewHandler()
+	// pasporHandler := ph.NewHandler()
+	// visaHandler := vh.NewHandler()
+	// intalHandler := ih.NewHandler()
+	// pnbpHandler := pnbph.NewHandler()
+	// satkerHandler := sh.NewHandler()
 
-	v1 := r.Group("/v1")
-	{
-		v1.POST("/login", generalHandler.Login)
+	// v1 := r.Group("/v1")
+	// {
+	// 	v1.POST("/login", generalHandler.Login)
 
-		resources := v1.Group("/resources")
-		{
-			resources.GET("/user/:id", userHandler.GetOneByID)
-			resources.GET("/satker", satkerHandler.GetAllSatker)
-			resources.POST("/filter-monthyear/", satkerHandler.GetReportMonthYear)
-			resources.POST("/get-pnbp/", pasporHandler.GetPnbpPaspor)
-			resources.POST("/get-PivotPerwilayah/", pnbpHandler.GetPivotPerwilayah)
+	// 	resources := v1.Group("/resources")
+	// 	{
+	// 		resources.GET("/user/:id", userHandler.GetOneByID)
+	// 		resources.GET("/satker", satkerHandler.GetAllSatker)
+	// 		resources.POST("/filter-monthyear/", satkerHandler.GetReportMonthYear)
+	// 		resources.POST("/get-pnbp/", pasporHandler.GetPnbpPaspor)
+	// 		resources.POST("/get-PivotPerwilayah/", pnbpHandler.GetPivotPerwilayah)
 
-			resources.POST("/paspor-by/", pasporHandler.GetAllByDate)
-			resources.GET("/paspor-pivot-perwilayah", pasporHandler.GetPivotPerwilayah)
-			resources.GET("/paspor-byKelaminPer10hari/:id_kantor", pasporHandler.GetKelaminPer10hari)
+	// 		resources.POST("/paspor-by/", pasporHandler.GetAllByDate)
+	// 		resources.GET("/paspor-pivot-perwilayah", pasporHandler.GetPivotPerwilayah)
+	// 		resources.GET("/paspor-byKelaminPer10hari/:id_kantor", pasporHandler.GetKelaminPer10hari)
 
-			resources.POST("/visa-by/tanggal", visaHandler.GetAllByDate)
-			//resources.GET("/visa-by/:variable/:value", visaHandler.GetAllByDate)
+	// 		resources.POST("/visa-by/tanggal", visaHandler.GetAllByDate)
+	// 		//resources.GET("/visa-by/:variable/:value", visaHandler.GetAllByDate)
 
-			resources.POST("/intal-by/", intalHandler.GetAllByDate)
-			resources.GET("/intal-pivot-perwilayah", intalHandler.GetPivotPerwilayah)
-			resources.GET("/intal-byKelaminPer10hari/:id_kantor", intalHandler.GetKelaminPer10hari)
+	// 		resources.POST("/intal-by/", intalHandler.GetAllByDate)
+	// 		resources.GET("/intal-pivot-perwilayah", intalHandler.GetPivotPerwilayah)
+	// 		resources.GET("/intal-byKelaminPer10hari/:id_kantor", intalHandler.GetKelaminPer10hari)
 
-			resources.GET("/pnbp-kategori", kategoriPNBPHandler.GetAllByParent)
-			resources.POST("/pnbp-by/", pnbpHandler.GetAllByDate)
-			resources.GET("/pnbp-perbulantahun", pnbpHandler.GetAllkategoriPNBPPerbulanTahun)
-			resources.GET("/pnbp-total", pnbpHandler.GetTotalPnbp)
-			resources.GET("/pnbp-byKelaminPer10hari/:id_kantor", pnbpHandler.GetKelaminPer10hari)
-		}
-	}
+	// 		resources.GET("/pnbp-kategori", kategoriPNBPHandler.GetAllByParent)
+	// 		resources.POST("/pnbp-by/", pnbpHandler.GetAllByDate)
+	// 		resources.GET("/pnbp-perbulantahun", pnbpHandler.GetAllkategoriPNBPPerbulanTahun)
+	// 		resources.GET("/pnbp-total", pnbpHandler.GetTotalPnbp)
+	// 		resources.GET("/pnbp-byKelaminPer10hari/:id_kantor", pnbpHandler.GetKelaminPer10hari)
+	// 	}
+	// }
 
 	return r
 }
